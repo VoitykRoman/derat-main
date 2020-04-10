4eccee01031702546516a8beb9c1aad1b32aa6e7
Роман Войтик <voytik.roman21@gmail.com>
2020-04-10 16:08:13 +0300

    updated base url

--- src/app/constants/http.base.url.constant.ts
+++ src/app/constants/http.base.url.constant.ts
@@ -1,5 +1,6 @@
 export class HttpBaseUrl{
-     static httpBaseUrl: string = 'http://192.168.1.200:5000';
+     static httpBaseUrl: string = 'http://52.58.135.192';
     
+     
     //  static httpBaseUrl: string = 'http://192.168.1.34:5000';
 }
