70fcc4f1e1f532396303852a4b1bfc81654a3126
Роман Войтик <voytik.roman21@gmail.com>
2020-04-14 14:42:30 +0300

    remove delete button on facility page

--- src/app/constants/http.base.url.constant.ts
+++ src/app/constants/http.base.url.constant.ts
@@ -2,5 +2,5 @@ export class HttpBaseUrl{
      static httpBaseUrl: string = 'http://52.58.135.192';
     
      
-    //  static httpBaseUrl: string = 'http://192.168.1.34:5000';
+    //  static httpBaseUrl: string = 'http://localhost:5000';
 }
